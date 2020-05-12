4ce0ad2e8325c2972825d9c5cccd74ebe47fe100
malvesbertoni <malvesbertoni@gmail.com>
2020-05-11 21:59:19 -0300

    Added 'key' command

---
+++ bin/cryprice.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+const program = require('commander');
+const pkg = require('../package.json');
+
+program
+    .version(pkg.version)
+    .command('key', 'Manage API key -- https://nomics.com')
+    .parse(process.argv);
