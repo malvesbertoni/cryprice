73205259fddedcc44773918e5b77ef314aedb148
malvesbertoni <malvesbertoni@gmail.com>
2020-05-13 20:16:43 -0300

    Added 'set' function

--- commands/key.js
+++ commands/key.js
@@ -1,6 +1,25 @@
+const inquirer = require('inquirer');
+const colors = require('colors');
+const KeyManager = require('../lib/KeyManager');
+const { isRequired } = require('../utils/validation');
+
 const key = {
-    set() {
-        console.log("SET");
+    async set() {
+        const keyManager = new KeyManager();
+        const input = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'key',
+                message: 'Enter API key'.green + ' https://nomics.com',
+                validate: isRequired
+            }
+        ]);
+
+        const key = keyManager.setKey(input.key);
+
+        if (key) {
+            console.log('API key has been set.'.blue);
+        }
     },
     show() {
         console.log("SHOW");
