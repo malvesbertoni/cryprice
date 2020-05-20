76268e7f7b567f380f79ca6f1a1839225b8172c7
malvesbertoni <malvesbertoni@gmail.com>
2020-05-20 17:11:57 -0300

    Added comments

--- bin/cryprice.js
+++ bin/cryprice.js
@@ -1,7 +1,14 @@
 #!/usr/bin/env node
+
+//----------------------------- PROGRAM CORE FILE ---------------------------------
+// Establishes the 'cryprice' main command and its sub-commands, 'key' and 'check'.
+//---------------------------------------------------------------------------------
+
+// Loading requirements
 const program = require('commander');
 const pkg = require('../package.json');
 
+// Establishing the main command
 program
     .version(pkg.version)
     .command('key', 'Manage API key -- https://nomics.com')
