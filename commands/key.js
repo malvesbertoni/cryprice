e49f4ea37bc695d1b642033672072ad0ade08b43
malvesbertoni <malvesbertoni@gmail.com>
2020-05-20 17:12:20 -0300

    Added comments

--- commands/key.js
+++ commands/key.js
@@ -1,9 +1,19 @@
+//----------------------------------- KEY COMMAND FILE ---------------------------------
+// Responsible for the creating the functions related to the 'key' child-commands.
+// set() - Receives the user's key via input and stores it, using the KeyManager class.
+// show() - Retrieves the user's key previously stored, using the KeyManager class.
+// remove() - Removes the previously stored key, using the KeyManager class.
+//--------------------------------------------------------------------------------------
+
+// Loading requirements
 const inquirer = require('inquirer');
 const colors = require('colors');
 const KeyManager = require('../lib/KeyManager');
 const { isRequired } = require('../utils/validation');
 
 const key = {
+    // Instantiates a new KeyManager object, receives the user's key via input
+    // and use the 'setKey(key)' method from the KeyManager class to store it
     async set() {
         const keyManager = new KeyManager();
         const input = await inquirer.prompt([
@@ -21,6 +31,9 @@ const key = {
             console.log('API key has been set.'.blue);
         }
     },
+
+    // Instantiates a new KeyManager object, and uses the 'getKey()'
+    // method from the KeyManager class to retrieve the stored key
     show() {
         try {
             const keyManager = new KeyManager();
@@ -33,6 +46,9 @@ const key = {
             console.error(error.message.red);
         }
     },
+
+    // Instantiates a new KeyManager object, and uses the 'deleteKey()'
+    // method from the KeyManager class to remove the stored key
     remove() {
         try {
             const keyManager = new KeyManager();
@@ -48,4 +64,5 @@ const key = {
     }
 }
 
+// Exporting the file
 module.exports = key;
