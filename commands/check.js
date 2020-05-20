e5c93bdcd532d4dbe17af640b5063c3f618fc2c6
malvesbertoni <malvesbertoni@gmail.com>
2020-05-20 17:12:08 -0300

    Added comments

--- commands/check.js
+++ commands/check.js
@@ -1,7 +1,18 @@
+//----------------------------------- CHECK COMMAND FILE -----------------------------------------
+// Responsible for the creating the function related to the 'check' child-command.
+// price(cmd) - Using the previously stored key, via the KeyManager class, it communicates 
+//    with the API, through the NomicsAPI class, and returns and prints the cryptocurrency prices. 
+//    This method can receive two arguments, coin and currency, to modify the returned content.
+//------------------------------------------------------------------------------------------------
+
+// Loading requirements
 const KeyManager = require('../lib/KeyManager');
 const NomicsAPI = require('../lib/NomicsAPI');
 
 const check = {
+    // Instantiates a new KeyManager object and retrieves the stored key.
+    // Instantiates a new NomicsAPI object and, using the key, communicates
+    // with the API. It then prints the returned data. Method receive two args.
     async price(cmd) {
         try {
             const keyManager = new KeyManager();
@@ -18,4 +29,5 @@ const check = {
     }
 };
 
+// Exporting the file
 module.exports = check;
